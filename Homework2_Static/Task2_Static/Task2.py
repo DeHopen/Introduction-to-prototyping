a8031c3f5f7d4aaa60adf57f086f231d5197a7db
Gerasimov Andrey <46645241+DeHopen@users.noreply.github.com>
2024-07-02 22:02:03 +0300

    Add files via upload

---
+++ Homework2_Static/Task2_Static/Task2.py
@@ -0,0 +1,62 @@
+import time
+import mujoco
+import numpy as np
+import pandas as pd
+from itertools import product
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# Function to convert degrees to radians
+def deg2rad(degrees):
+    return np.deg2rad(degrees)
+
+
+# Joint limits and step size
+joint_limits = np.arange(-90, 91, 15)  # -90 to 90 degrees in 15-degree increments
+
+# Prepare the CSV file
+results = []
+
+# Load the model
+model = mujoco.MjModel.from_xml_path('Task2.xml')
+data = mujoco.MjData(model)
+
+# Get all possible joint angle configurations
+all_configs = list(product(joint_limits, repeat=model.njnt))
+
+# Iterate over all configurations
+for config in all_configs:
+    # Set joint angles
+    data.qpos[:] = [deg2rad(angle) for angle in config]
+
+    # Compute inverse dynamics
+    mujoco.mj_inverse(model, data)
+
+    # Save the configuration and resulting torques
+    results.append({
+        'joint1_angle': config[0],
+        'joint2_angle': config[1],
+        'joint3_angle': config[2],
+        'torque1': data.qfrc_inverse[0],
+        'torque2': data.qfrc_inverse[1],
+        'torque3': data.qfrc_inverse[2],
+    })
+
+# Convert results to a DataFrame and save to a CSV file
+df = pd.DataFrame(results)
+df.to_csv('inverse_dynamics_results.csv', index=False)
+
+print('Results saved to inverse_dynamics_results.csv')
+
+# Create a violin plot for the torque distributions
+torque_data = pd.melt(df, value_vars=['torque1', 'torque2', 'torque3'],
+                      var_name='joint', value_name='torque')
+
+plt.figure(figsize=(10, 6))
+sns.violinplot(x='joint', y='torque', data=torque_data)
+plt.title('Distribution of Torques in Different Joints')
+plt.xlabel('Joint')
+plt.ylabel('Torque')
+plt.grid(True)
+plt.show()
